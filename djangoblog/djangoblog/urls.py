6466d1ce7706ad211ccaf2c597f3ee809f006821
Magomed Shamaev <ms77grz@gmail.com>
2020-09-27 21:09:11 +0300

    fixed url

--- djangoblog/djangoblog/urls.py
+++ djangoblog/djangoblog/urls.py
@@ -34,7 +34,7 @@ urlpatterns = [
         template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
     path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(
         template_name='users/password_reset_complete.html'), name='password_reset_complete'),
-    path('', include('blog.urls')),
+    path('blog/', include('blog.urls')),
 ]
 
 if settings.DEBUG:
